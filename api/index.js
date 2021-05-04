5ff4d52d7860de1dc9fa659bf1cc4944f9b8af44
zomars <zomars@me.com>
2021-05-04 15:27:40 -0600

    Adds log

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const { join } = require("path");
 
 const pathToFile = join(tmpdir(), "db.json");
 const server = jsonServer.create();
+console.log(`pathToFile`, pathToFile);
 const router = jsonServer.router(pathToFile);
 const middlewares = jsonServer.defaults({ readOnly: true });
 const port = process.env.PORT || 80;
