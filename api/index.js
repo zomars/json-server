e1c2b8bf1a90602fd52d5a4f4f5b576b50c7f83e
zomars <zomars@me.com>
2021-05-04 15:43:32 -0600

    Debugging

--- api/index.js
+++ api/index.js
@@ -5,6 +5,14 @@ const { join } = require("path");
 const pathToFile = join(tmpdir(), "db.json");
 const server = jsonServer.create();
 console.log(`pathToFile`, pathToFile);
+
+fs.readFile(pathToFile, "utf8", (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
+
 const router = jsonServer.router(pathToFile);
 const middlewares = jsonServer.defaults({ readOnly: true });
 const port = process.env.PORT || 80;
