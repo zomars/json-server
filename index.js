68c51b5166ec2f2ed75253bdd51efe29157ed5c6
zomars <zomars@me.com>
2021-05-04 14:29:03 -0600

    Prepares for vercel deploy

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const faker = require('faker');
+const faker = require("faker");
 
-let database = { employers: []};
+let database = { employers: [] };
 const threshold = 1000;
 
-for (let i = 1; i<= threshold; i++) {
+for (let i = 1; i <= threshold; i++) {
   database.employers.push({
     id: i,
     name: faker.name.firstName() + " " + faker.name.lastName(),
@@ -14,10 +14,11 @@ for (let i = 1; i<= threshold; i++) {
     city: faker.address.city(),
     state: faker.address.state(),
     zip: faker.address.zipCode(),
-    country: 'US',
+    country: "US",
     phone: faker.phone.phoneNumber(),
     logo: faker.internet.avatar(),
-    description: 'Plumbing, Heating and Air conditioning Contractor serving Fairfield County',
+    description:
+      "Plumbing, Heating and Air conditioning Contractor serving Fairfield County",
     head_count: faker.datatype.number(),
     is_verified: faker.datatype.boolean(),
   });
